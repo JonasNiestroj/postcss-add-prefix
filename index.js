79995e271397c8d3a3bff5640dc21f0d321c277a
Jonas Niestroj <j.niestroj@protonmail.ch>
2020-07-03 10:15:07 +0200

    Add basic prefix selector plugin

---
+++ index.js
@@ -0,0 +1,22 @@
+var postcss = require("postcss");
+
+const checkForKeyframe = (selector) => {
+  return /^([0-9]*[.])?[0-9]+\%$|^from$|^to$/.test(selector);
+};
+
+const walkRules = (postcss) => {
+  postcss.walkRules((rule) => {
+    rule.selectors = rule.selectors.map((selector) => {
+      // Check if the current rule is a keyframe
+      if (checkForKeyframe(selector)) {
+        return selector;
+      }
+      return `${options.selector} ${selector}`;
+    });
+  });
+};
+
+module.exports = postcss.plugin("postcss-prefix-selector", (options) => {
+  options = options || { selector: "#prefix" };
+  return (postcss) => walkRules(postcss);
+});
