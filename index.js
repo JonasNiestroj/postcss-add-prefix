6c5ca65f7aed8100ae48fcf50ab70f819981a74d
Jonas Niestroj <j.niestroj@protonmail.ch>
2020-07-07 11:48:48 +0200

    Add exclude and no-prefix atrule

--- index.js
+++ index.js
@@ -1,22 +1,70 @@
 var postcss = require("postcss");
 
+const compiledExcludes = [];
+let noPrefix = false;
+
 const checkForKeyframe = (selector) => {
   return /^([0-9]*[.])?[0-9]+\%$|^from$|^to$/.test(selector);
 };
 
-const walkRules = (postcss, options) => {
-  postcss.walkRules((rule) => {
-    rule.selectors = rule.selectors.map((selector) => {
-      // Check if the current rule is a keyframe
-      if (checkForKeyframe(selector)) {
-        return selector;
-      }
-      return `${options.selector} ${selector}`;
+const compileExcludes = (excludes) => {
+  if (Array.isArray(excludes)) {
+    excludes.forEach((exclude) => {
+      compiledExcludes.push(new RegExp(exclude));
     });
+  } else {
+    compiledExcludes.push(new RegExp(excludes));
+  }
+};
+
+const checkForExclude = (selector) => {
+  for (let i = 0; i < compiledExcludes.length; i++) {
+    const match = compiledExcludes[i].test(selector);
+    if (match) {
+      return true;
+    }
+  }
+  return false;
+};
+
+const walkRules = (postcss, result, options) => {
+  if (options.exclude) {
+    compileExcludes(options.exclude);
+  }
+
+  postcss.each((rule) => {
+    if (rule.type === "atrule") {
+      if (rule.name === "no-prefix") {
+        noPrefix = true;
+      } else if (rule.name === "end-no-prefix") {
+        noPrefix = false;
+      }
+    } else if (rule.type === "rule") {
+      if (!noPrefix) {
+        const selector = rule.selector;
+        // Check if the current rule is a keyframe
+        if (checkForKeyframe(selector)) {
+          return selector;
+        }
+
+        if (options.exclude && checkForExclude(selector)) {
+          return selector;
+        }
+
+        if (selector.includes(",")) {
+          const split = selector.split(",");
+          rule.selector = split
+            .map((sel) => `${options.selector} ${sel}`)
+            .join(",");
+        } else {
+          rule.selector = `${options.selector} ${selector}`;
+        }
+      }
+    }
   });
 };
 
 module.exports = postcss.plugin("postcss-add-prefix", (options) => {
   options = options || { selector: "#prefix" };
-  return (postcss) => walkRules(postcss, options);
+  return (postcss, result) => walkRules(postcss, result, options);
 });
